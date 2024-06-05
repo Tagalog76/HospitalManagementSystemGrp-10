603d5917ab65600dfd9bb9836b964e335584431e
Tagalog <tagaloglorenzo76@gmail.com>
2024-06-05 14:57:07 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\DoctorsController;
+use App\Http\Controllers\PatientsController;
 
 Route::middleware('auth:api')->group(function () {
     
@@ -16,5 +17,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('appointments', Appointments::class);
-Route::apiResource('users', Users::class);
+Route::apiResource('appointments', AppointmentsController::class);
+Route::apiResource('users', UsersController::class);
+Route::apiResource('doctors', DoctorsController::class);
+Route::apiResource('doctors', PatientsController::class);
