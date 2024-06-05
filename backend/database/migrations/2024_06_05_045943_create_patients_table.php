5f9bbd6395034533d264b003dba84b9949620920
LA Santos <123438824+BlankPhantom@users.noreply.github.com>
2024-06-05 14:04:50 +0800

    Update 2024_06_05_045943_create_patients_table.php

--- backend/database/migrations/2024_06_05_045943_create_patients_table.php
+++ backend/database/migrations/2024_06_05_045943_create_patients_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
 
         Schema::create('patients', function (Blueprint $table) {   
-            $table->id();
+            $table->id('patient_id');
             $table->string('first_name');
             $table->string('last_name');
             $table->date('date_of_birth');
