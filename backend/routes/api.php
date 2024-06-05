6d3e26f52d4fa7f6670d0fcbc84e5b6484fa8fe6
Ace Sumaoy <sumaoyace78@gmail.com>
2024-06-05 14:52:42 +0800

    Update api doctors

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\Doctors;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DoctorsController;
 
 Route::middleware('auth:api')->group(function () {
+<<<<<<< Updated upstream
     
+=======
+    Route::apiResource('appointments', AppointmentController::class);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('doctors', Doctors::class);
+>>>>>>> Stashed changes
 });
 
 
