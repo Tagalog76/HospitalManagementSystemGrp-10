a5eb990900be51ae541b9c983ad9ae24b128fe0e
Tagalog <tagaloglorenzo76@gmail.com>
2024-06-05 14:54:35 +0800

    Update PatientSeeder.php

--- backend/database/seeders/PatientSeeder.php
+++ backend/database/seeders/PatientSeeder.php
@@ -1,39 +1,42 @@
 <?php
-use App\Models\Patient;
-use Illuminate\Database\Seeder;
 
-class PatientSeeder extends Seeder
-{
-    public function run()
+    namespace Database\Seeders;
+    use App\Models\Patient;
+    use Illuminate\Database\Seeder;
+    use Illuminate\Support\Facades\DB;
+
+    class PatientSeeder extends Seeder
     {
-        $patients = [
-            [
-                'first_name' => 'Zyrell',
-                'last_name' => 'Trinidad',
-                'date_of_birth' => '2002-01-15',
-                'gender' => 'male',
-                'address' => '123 Main St, City, Country',
-                'phone' => '1234567890',
-                'email' => 'zyrell@gmail.com',
-                'emergency_contact' => 'Josh Trinidad',
-                'medical_history' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ],
- [
-                'first_name' => 'Zyrell',
-                'last_name' => 'Trinidad',
-                'date_of_birth' => '2002-01-15',
-                'gender' => 'male',
-                'address' => '123 Main St, City, Country',
-                'phone' => '1234567890',
-                'email' => 'zyrell@gmail.com',
-                'emergency_contact' => 'Josh Trinidad',
-                'medical_history' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            ]
-            
-        ];
+        public function run()
+        {
+            $patients = [
+                [
+                    'first_name' => 'Zyrell',
+                    'last_name' => 'Trinidad',
+                    'date_of_birth' => '2002-01-15',
+                    'gender' => 'male',
+                    'address' => '123 Main St, City, Country',
+                    'phone' => '1234567890',
+                    'email' => 'zyrell@gmail.com',
+                    'emergency_contact' => 'Josh Trinidad',
+                    'medical_history' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                ],
+    [
+                    'first_name' => 'Zyrell',
+                    'last_name' => 'Trinidad',
+                    'date_of_birth' => '2002-01-15',
+                    'gender' => 'male',
+                    'address' => '123 Main St, City, Country',
+                    'phone' => '1234567890',
+                    'email' => 'zyrell@gmail.com',
+                    'emergency_contact' => 'Josh Trinidad',
+                    'medical_history' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                ]
+                
+            ];
 
-        foreach ($patients as $patientData) {
-            Patient::create($patientData);
+            foreach ($patients as $patientData) {
+                Patient::create($patientData);
+            }
         }
     }
-}
