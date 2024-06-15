d75f1685996be407aeb8aea5642dbc633ad3d926
Tagalog76 <tagaloglorenzo76@gmail.com>
2024-06-15 12:18:24 +0800

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home.js';
-import Register from './components/Register.js';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import Home from './pages/Home';
+import Register from './components/Register';
 import Login from './components/Login';
 import AdminDashboard from './components/AdminDashboard';
 import DoctorDashboard from './components/DoctorDashboard';
@@ -10,27 +10,25 @@ import PatientDashboard from './components/PatientDashboard';
 import ManagePatients from './pages/ManagePatients';
 import ManageDoctors from './pages/ManageDoctors';
 import BookAppointment from './pages/BookAppointment';
-import ViewAppointments from './pages/ViewAppointment.js';
+import ViewAppointments from './pages/ViewAppointment';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Route>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-          <Route path="/patient-dashboard" element={<PatientDashboard />} />
-          <Route path="/book-appointment" element={<BookAppointment />} />
-          <Route path="/manage-doctors" element={<ManageDoctors />} />
-          <Route path="/manage-patients" element={<ManagePatients />} />
-          <Route path="/view-appointment" element={<ViewAppointments />} />
-        </Route>
-      </div>
-    </Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/pages/Home" element={<Home />} />
+        <Route path="/components/Register" element={<Register />} />
+        <Route path="/components/Login" element={<Login />} />
+        <Route path="/components/AdminDashboard" element={<AdminDashboard />} />
+        <Route path="/components/DoctorDashboard" element={<DoctorDashboard />} />
+        <Route path="/components/PatientDashboard" element={<PatientDashboard />} />
+        <Route path="/pages/ManagePatients" element={<ManagePatients />} />
+        <Route path="/pages/ManageDoctors" element={<ManageDoctors />} />
+        <Route path="/pages/BookAppointment" element={<BookAppointment />} />
+        <Route path="/pages/ViewAppointments" element={<ViewAppointments />} />
+      </Routes>
+    </>
   );
 };
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -16,4 +15,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+
